aced179766283ad7ecbd3006ab4aa266c58bcf83
Anees ur Rehman <aneesurrehman1358@gmail.com>
2025-03-04 02:22:33 +0500

    Update firebase.js

--- frontend/src/components/firebase.js
+++ frontend/src/components/firebase.js
@@ -5,12 +5,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZsllAnWqCGMFOt4saTbYGxHBK_gQ_KxU",
-  authDomain: "campussportssphere-7f23f.firebaseapp.com",
-  projectId: "campussportssphere-7f23f",
-  storageBucket: "campussportssphere-7f23f.appspot.com",
-  messagingSenderId: "661686787234",
-  appId: "1:661686787234:web:7aa7c969e1941fd9650bfb",
+ // your data here of firebase.
 };
 
 const app = initializeApp(firebaseConfig);
